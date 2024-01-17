9e71a319b8e00a44647aa6ee6b2b790bd94c33f3
BrysonPotts007 <BrysonPotts007@proton.me>
2024-01-17 12:28:05 +0800

    Deprecate user balances storage. Deposit to place an order & Deposit to make a deal.

--- otc-market-contract.lua
+++ otc-market-contract.lua
@@ -7,8 +7,9 @@ type Storage = {
     quote: string, 
     state: string,
     owner: string,
-	order_idx: int,
-	assets_map: Map<string>,   -- Key: User Address   Value: assets json str
+	order_index: int,
+    orders_count: int,
+	-- assets_map: Map<string>,   -- Key: User Address   Value: assets json str
 	sell_orders: Map<string>    -- Key: Order Idx    Value: order info
 } 
 
@@ -20,8 +21,9 @@ function M:init()
     self.quote_symbol = "BTC"
     self.storage.state = 'N'
     self.storage.owner = caller_address
-    self.storage.order_idx = 0
-	self.storage.assets_map = {}
+    self.storage.order_index = 1
+    self.storage.order_count = 0
+	-- self.storage.assets_map = {}
 	self.storage.sell_orders = {} 
 end
 
@@ -46,36 +48,67 @@ end
 
 
 -- If you want to sub it, use negative amount
-let function add_asset_amount(M: table, user: string, symbol: string, amount: int)
-    var user_assets_str = self.storage.assets_map[user]
-    var user_assets = {}
-    var after_amount = 0
-    if (user_assets_str == nil) then
-        if (amount <= 0) then
-            return error("Asset amount can not be negative")
-        else
-            after_amount = amount
-        end
-    else
-        user_assets = json.loads(user_assets_str)
-        let previous_amount = user_assets[symbol]
-        if (previous_amount == nil) then
-            if (amount <= 0) then
-                return error("Asset amount can not be negative")
-            else
-                after_amount = amount
-            end
-        else
-            after_amount = previous_amount + amount
-            if (after_amount <= 0) then 
-                return error("Asset amount can not be negative")
-            end
-        end
-    end
+-- let function add_asset_amount(M: table, user: string, symbol: string, amount: int)
+--     var user_assets_str = self.storage.assets_map[user]
+--     var user_assets = {}
+--     var after_amount = 0
+--     if (user_assets_str == nil) then
+--         if (amount <= 0) then
+--             return error("Asset amount can not be negative")
+--         else
+--             after_amount = amount
+--         end
+--     else
+--         user_assets = json.loads(user_assets_str)
+--         let previous_amount = user_assets[symbol]
+--         if (previous_amount == nil) then
+--             if (amount <= 0) then
+--                 return error("Asset amount can not be negative")
+--             else
+--                 after_amount = amount
+--             end
+--         else
+--             after_amount = previous_amount + amount
+--             if (after_amount <= 0) then 
+--                 return error("Asset amount can not be negative")
+--             end
+--         end
+--     end
 
-    user_assets[symbol] = after_amount
-    let r = json.dumps(user_assets)
-    self.storage.asset_map[user] = r
+--     user_assets[symbol] = after_amount
+--     let r = json.dumps(user_assets)
+--     self.storage.asset_map[user] = r
+-- end
+
+
+offline function M:market_pair(_: string)
+    return self.storage.base_symbol .. "-" .. self.storage.quote_symbol
+end
+
+
+offline function M:state(_: string)
+    return self.storage.state
+end
+
+
+offline function M:owner(_: string)
+    return self.storage.owner
+end
+
+
+offline function M:order_index(_: string)
+    return self.storage.order_index
+end
+
+
+offline function M:order_count(_: string)
+    return self.storage.order_count
+end
+
+
+offline function M:sell_orders(_: string)
+    let sell_orders_str = json.dumps(self.storage.sell_orders)
+    return sell_orders_str
 end
 
 
